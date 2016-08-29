94c575b0103b68838e28c6dfdf67f92fcc359ee8
Neil Marlowe <nmarlowe@gmail.com>
2016-08-28 21:54:40 -0400

    Fix contact form

--- js/form.js
+++ js/form.js
@@ -90,20 +90,3 @@ $.fn.goValidate = function() {
     return this;
 };
 $('form').goValidate();
-// 
-// $(document).ready(function () {
-//     $("input#submit").click(function(){
-//         $.ajax({
-//             type: "POST",
-//             url: "process.php", //process to mail
-//             data: $('form.contact').serialize(),
-//             success: function(msg){
-//                 $("#thanks").html(msg) //hide button and show thank you
-//                 $("#form-content").modal('hide'); //hide popup
-//             },
-//             error: function(){
-//                 alert("failure");
-//             }
-//         });
-//     });
-// });
--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,5 @@
+// Script to redirect to main page after 5 seconds
+
 $(document).ready(function () {
   var timer = setTimeout(function() {
             window.location='index.html'
